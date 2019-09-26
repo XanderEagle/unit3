46edfc460f56044869ae8b2ea1afd227a7074378
Xander <alexander.eagle@ssfs.org>
2019-09-26 14:16:21 -0400

    made slash hexagon

---
+++ classwork.py
@@ -0,0 +1,27 @@
+def hexagonb():
+    print("  __________")
+    print(" /          \\")
+    print("/            \\")
+
+
+def hexagont():
+    print("\\            /")
+    print(" \\          /")
+    print("  __________")
+
+
+def bottom():
+    print("_''_'_''_'_''")
+
+
+hexagonb()
+hexagont()
+bottom()
+hexagonb()
+hexagont()
+bottom()
+hexagont()
+hexagonb()
+bottom()
+hexagont()
+
