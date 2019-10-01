206c8ccc22a0698c1d5123c2e416edb9cd5c6452
Xander <alexander.eagle@ssfs.org>
2019-10-01 08:59:46 -0400

    making the function plus sign

---
+++ unit3.py
@@ -0,0 +1,15 @@
+import turtle
+
+def plus_sign():
+    turtle.fd(40)
+    turtle.left(90)
+    turtle.fd(80)
+    turtle.right(90)
+    turtle.fd(40)
+    turtle.left(90)
+    turtle.fd(80)
+    turtle.right(90)
+
+plus_sign()
+
+turtle.exitonclick()
